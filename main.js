f896e0b821b755e406c03e0e562c3e3a3acc10c9
Christopher Hutchinson <cshutchinson@gmail.com>
2015-11-13 11:36:15 -0700

    one promise working

--- main.js
+++ main.js
@@ -16,23 +16,23 @@ function insertCards(count){
   }
 }
 
-function addImageToCards(){
-  $('.back').each(function(index){
-    $('<img>').attr('src', cardImages[index]).appendTo(this);
-  });
+function addImageToCard(data){
+  console.log($('.back:last').length);
+  $('.back:last').append($('<img>').attr('src', data));
 }
 
 
 function retrieveImage(){
   return new Promise(function(succeed, fail){
     var req = new XMLHttpRequest();
+    req.responseType = 'arraybuffer';
     req.open('GET', 'http://lorempixel.com/70/70/sports/', true);
-    req.addEventListener('load', function(){
+    req.onload = function(){
       if (req.status < 400)
-        succeed(req.responseText);
+        succeed(req.response);
       else
         fail(new Error('Request failed: ' + req.statusText));
-    });
+    };
     req.addEventListener('error', function(){
       fail(new Error('Network error'));
     });
@@ -40,25 +40,30 @@ function retrieveImage(){
   });
 }
 
-retrieveImage().then(function(text){
-  var arr = new Uint8Array(text);
-  var raw = String.fromCharCode.apply(null, arr);
-  var b64 = btoa(raw);
-  var dataURL='data:image/jpeg;base64,'+b64;
-  // $('<img>').attr('src', dataURL).appendTo(document.body)
-  console.log(arr);
-}, function(error){
-  console.log('Failed to fetch image: ' + error);
-});
+function imageReady(text){
+  
+}
+
+
+  retrieveImage().then(function(text){
+    var arr = new Uint8Array(text);
+    var raw = String.fromCharCode.apply(null, arr);
+    var b64 = btoa(raw);
+    var dataURL='data:image/jpeg;base64,'+b64;
+    // $('<img>').attr('src', dataURL).appendTo(document.body)
+    insertCards(1);
+    addImageToCard(dataURL);
+    $('.card').click(function(){
+      $(this).toggleClass('flip');
+    });
+  }, function(error){
+    console.log('Failed to fetch image: ' + error);
+  });
 
 
 
 
 
-
-  //
-  // insertCards(2);
-  // addImageToCards();
-  // $('.card').click(function(){
-  //   $(this).toggleClass('flip');
-  // });
+// insertCards(2);
+// console.log(cardImages);
+// addImageToCards();
