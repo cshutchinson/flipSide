6b23846150c4108137d99853e77c97ef9b2072fe
Christopher Hutchinson <cshutchinson@gmail.com>
2015-11-14 15:48:40 -0700

    gulp implemented with mocha, webpack

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var webpack = require('gulp-webpack');
+var mocha = require('gulp-mocha');
+
+gulp.task('default', ['webpack', 'css', 'test'], function() {
+  return gulp
+    .src('*.html')
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('css', function() {
+  return gulp
+    .src('*.css')
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('webpack', function() {
+  return gulp
+  .src('game.js')
+  .pipe(webpack({
+    output: {
+      filename: 'bundle.js',
+    },
+  }))
+  .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('test', function () {
+    return gulp
+    .src('./test/*.js', {read: false})
+    .pipe(mocha());
+});
