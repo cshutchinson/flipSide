83545a5e717d2cca3fd8be841952c6eab0edb193
Christopher Hutchinson <cshutchinson@gmail.com>
2015-11-15 13:15:26 -0700

    can identify matching cards and adding new .matched class

--- play.js
+++ play.js
@@ -1,17 +1,19 @@
 module.exports = {
   checkForMatch(){
     var flippedCards = $('div.card.flip>div.face.back>img');
-    if (flippedCards.count === 2 ){
+    if (flippedCards.length === 2 ){
       // TODO: remove click event from all cards
 
       //check to see that the src of the images are equal
-      if (flippedCards[0].src = flippedCards[1].src){
-        flippedCards.addClass('matched');
-        flippedCards.removeClass('flipped');
+      if (flippedCards[0].src === flippedCards[1].src){
+        flippedCards.parent().parent().addClass('matched');
+        flippedCards.parent().parent().removeClass('flip');
         // TODO: adjust score
+        console.log('increased score');
       } else {
-        flippedCards.removeClass('flipped');
+        flippedCards.parent.parent.removeClass('flip');
         // TODO: adjust score down
+        console.log('descreased score, no match');
       }
 
       // TODO: if all cards match end game
