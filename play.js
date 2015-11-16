9d27480603004979ebc253ac05020d1053bed35b
Christopher Hutchinson <cshutchinson@gmail.com>
2015-11-16 16:51:52 -0700

    messages added

--- play.js
+++ play.js
@@ -10,6 +10,7 @@ var score = {
 }
 
 function gameLoop(){
+  $('.turnMessage>h2').replaceWith('<h2>' + 'Go! Time counts!' + '</h2>');
   setInterval(onTimerTick, 500); // 33 milliseconds = ~ 30 frames per sec
   function onTimerTick() {
     checkForMatch();
@@ -34,12 +35,13 @@ function checkForMatch(){
     if ($flippedCards[0].src === $flippedCards[1].src){
       $flippedCards.parent().parent().addClass('matched');
       $flippedCards.parent().parent().removeClass('flip');
-      // TODO: display message of successful match
+      $('.turnMessage>h2').replaceWith('<h2>' + matchMessage(true) + '</h2>');
+
       correctMatchScore();
     } else {
       $flippedCards.parent().parent().removeClass('flip');
       incorrectMatchScore();
-      // TODO: display message that cards were not match obviously
+      $('.turnMessage>h2').replaceWith('<h2>' + matchMessage(false) + '</h2>');
     }
     // TODO: if all cards match end game
     // TODO: end game
@@ -57,9 +59,7 @@ function calculateScore(score, correct){
     score.score -= turnScore;
     if (score.score < 0) score.score = 0;
   }
-  console.log(score.score);
   $('.scoreNumeric>h2').replaceWith('<h2>' + score.score.toFixed(0) + '</h2>');
-  // $( "div.second" ).replaceWith( "<h2>New heading</h2>" );
   score.endTime = 0;
   score.startTime = 0;
   return score;
@@ -75,6 +75,24 @@ function correctMatchScore() {
   score.consecutiveCorrect ++;
   score.consecutiveWrong = 0;
   calculateScore(score, true);
+}
+
+function matchMessage(match){
+  matchMessages = ['You are rocking it!', 'Great match.', 'Speed demon!',
+    'Your memory is astonishing!', 'Are you a genius?', 'Your parents must be\
+    proud!', 'Way to go!', 'I admire your finesse!', 'Your performance is impres\
+    sive!', 'Wow, a raise is in your future!', 'Your kids will be super \
+    intelligent!', 'Can you do that again?', 'Watch out your boss is coming!'];
+  failMessages = ['You can do better than this!', 'Sad...try again!', 'Keep \
+    trying!', 'Is this the best you can do?', 'Do you need another drink?',
+    'Did you drop an edible last night?', 'Good thing you found this game!',
+    'This is the worst performance ever!', 'Did you repeat kindergarten?',
+    'This exercise can really help you.'];
+  if (match){
+    return (matchMessages[Math.floor(Math.random()*matchMessages.length)]);
+  } else {
+    return (failMessages[Math.floor(Math.random()*failMessages.length)]);
+  }
 }
 
 module.exports = {
