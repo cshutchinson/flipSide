b8e3b664e1d2b0225f9b3078760f289d84f37192
Christopher Hutchinson <cshutchinson@gmail.com>
2015-11-29 17:54:26 -0700

    minor refactoring

--- gameLoop.js
+++ gameLoop.js
@@ -9,7 +9,7 @@ var score = {
   endTime: 0
 };
 
-clickTarget = null;
+mainTimer = null;
 
 function gameLoop(){
   // animation is complete now - make it possible to reveal a card
@@ -18,10 +18,10 @@ function gameLoop(){
   if (score.startTime === 0) {
     score.startTime = Date.now();
   };
-  var mainTimer = setInterval(checkForMatch, 50);
+  mainTimer = setInterval(checkForMatch, 30);
 }
 
-function checkForMatch(timer){
+function checkForMatch(){
   // this function handles game state managment
   var $flippedCards = $('div.card.flip').not($('div.card.matched'));
   // user flipped a card
@@ -43,14 +43,14 @@ function checkForMatch(timer){
     } else {
       setTimeout(function(){
         $flippedCards.removeClass('flip');
-      }, 300);
+      }, 300); // delay so user can see cards don't match
       incorrectMatchScore();
     }
   }
   // check to see if all cards are matched and end game
   var $matchedCards = $('div.card.matched');
   if ($matchedCards.length === $('div.card').length){
-    window.clearInterval(timer);
+    window.clearInterval(mainTimer);
     gameComplete();
   }
 }
@@ -83,23 +83,6 @@ function correctMatchScore() {
   calculateScore(score, true);
 }
 
-function matchMessage(match){
-  matchMessages = ['You are rocking it!', 'Great match.', 'Speed demon!',
-    'Your memory is astonishing!', 'Are you a genius?', 'Your parents must be\
-    proud!', 'Way to go!', 'I admire your finesse!', 'Your performance is\
-    impressive!', 'Wow, a raise is in your future!', 'Your kids will be super \
-    intelligent!', 'Can you do that again?', 'Watch out your boss is coming!'];
-  failMessages = ['You can do better than this!', 'Sad...try again!', 'Keep \
-    trying!', 'Is this the best you can do?', 'Do you need another drink?',
-    'This game can really help you.', 'Good thing you found this game!',
-    'This is the worst performance ever!', 'Did you repeat a grade?']
-    ;
-  if (match){
-    return (matchMessages[Math.floor(Math.random()*matchMessages.length)]);
-  } else {
-    return (failMessages[Math.floor(Math.random()*failMessages.length)]);
-  }
-}
 
 function gameComplete(){
   // display game complete message
